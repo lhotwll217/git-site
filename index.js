64af3839c401eb07cf2f94f342ada9d0bc0c1a71
Luke Otwell <lukehenryotwell@gmail.com>
2021-06-14 00:19:07 -0400

    Pushing new things in my own special way

---
+++ index.js
@@ -0,0 +1,4 @@
+const h2 = document.createElement('h2');
+h2.textContent = "Please don't leave my website yet. I know it isn't exciting, but please stay few more moments."
+
+document.querySelector('body').appendChild(h2);
